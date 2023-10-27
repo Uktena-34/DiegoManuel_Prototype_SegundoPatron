0e69a7fd0347e631194cc01b1ce0caa1ee62a4f0
Uktena-34 <dgomezamaton9@gmail.com>
2023-10-27 17:49:28 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiegoManuel_Prototype_SegundoPatron
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //como utilizarla
+            Animal oAnimal = new Animal() { Nombre = "Oveja Dolly", Patas = 4 };
+            Animal oAnimalClonado = oAnimal.Clone() as Animal;
+            oAnimalClonado.Patas = 5;
+
+            Console.WriteLine(oAnimal.Patas);
+
+
+        }
+    }
+}
